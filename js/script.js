456188496009987091a3f5fd80eac3fe880520c2
Johnny Austen <johnnydausten@gmail.com>
2021-03-18 16:06:23 +0000

    Create Mobile Nav Toggler

--- js/script.js
+++ js/script.js
@@ -1,8 +1,24 @@
-function myFunction() {
-  var x = document.getElementById("sidebar__menu");
-  if (x.style.display === "block") {
-    x.style.display = "none";
-  } else {
-    x.style.display = "block";
-  }
+// function myFunction() {
+//   var x = document.getElementById("sidebar__menu");
+//   if (x.style.display === "block") {
+//     x.style.display = "none";
+//   } else {
+//     x.style.display = "block";
+//   }
+// }
+
+let open = false;
+
+/* Set the width of the side navigation to 250px */
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+  document.querySelector(".main").style.marginLeft = "250px";
+  document.querySelector(".main").style.opacity = ".7";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0px";
+  document.querySelector(".main").style.marginLeft = "0";
+  document.querySelector(".main").style.opacity = "1";
 }
