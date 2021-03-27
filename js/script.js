86652274584b8b6a1336d146dec90ca87853711a
Johnny Austen <johnnydausten@gmail.com>
2021-03-27 10:34:52 +0000

    Fix Modal Window closing

--- js/script.js
+++ js/script.js
@@ -1,19 +1,15 @@
-// function myFunction() {
-//   var x = document.getElementById("sidebar__menu");
-//   if (x.style.display === "block") {
-//     x.style.display = "none";
-//   } else {
-//     x.style.display = "block";
-//   }
-// }
+const mainDiv = document.querySelector(".main");
 
-let open = false;
+let menuStatus = "closed";
 
 /* Set the width of the side navigation to 250px */
 function openNav() {
   document.getElementById("mySidenav").style.width = "250px";
   document.querySelector(".main").style.marginLeft = "250px";
   document.querySelector(".main").style.opacity = ".7";
+  setTimeout(function () {
+    menuStatus = "open";
+  }, 100);
 }
 
 /* Set the width of the side navigation to 0 */
@@ -21,4 +17,12 @@ function closeNav() {
   document.getElementById("mySidenav").style.width = "0px";
   document.querySelector(".main").style.marginLeft = "0";
   document.querySelector(".main").style.opacity = "1";
+  menuStatus = "closed";
 }
+
+// Close Mobile Nav when a user clicks outside
+mainDiv.addEventListener("click", () => {
+  if (menuStatus === "open") {
+    closeNav();
+  }
+});
